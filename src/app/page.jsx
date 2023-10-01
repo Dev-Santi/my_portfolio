7b8826da357ab0d1718bf4fbad9d418ae3ef783f
santinakc <santinakcfin@gmail.com>
2023-10-01 16:26:09 -0300

    bug with open graph

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,14 +5,6 @@ import Proyects from "@/components/Proyects";
 export default function Home() {
   return (
     <main>
-      <meta property='og:image' content='<generated>' />
-      <meta
-        property='og:image:alt'
-        content='Foto del sitio web de Santiago Larrosa'
-      />
-      <meta property='og:image:type' content='/card_imgs/portfolio.png' />
-      <meta property='og:image:width' content='1200' />
-      <meta property='og:image:height' content='680' />
       <div>
         <Header />
       </div>
