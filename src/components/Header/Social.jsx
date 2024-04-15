e76f4b95a18f2c7b4adf670026e9dceabc676150
dev-santi <bauzalarrosasantiago@gmail.com>
2024-04-15 20:25:44 -0300

    id para test con playtwrigth

--- src/components/Header/Social.jsx
+++ src/components/Header/Social.jsx
@@ -1,35 +1,36 @@
 import style from "./header.module.css";
 import {
-  BsInstagram,
-  BsGithub,
-  BsLinkedin,
-  BsFillEnvelopeAtFill,
-  BsWhatsapp,
+    BsInstagram,
+    BsGithub,
+    BsLinkedin,
+    BsFillEnvelopeAtFill,
+    BsWhatsapp,
 } from "react-icons/bs";
 
 function Social() {
-  return (
-    <div className={style.social}>
-      <a href='https://github.com/Dev-Santi' target='_blank'>
-        <BsGithub />
-      </a>
-      <a
-        href='https://www.linkedin.com/in/santiago-larrosa-bauz%C3%A1-99b740251/'
-        target='_blank'
-      >
-        <BsLinkedin />
-      </a>
-      <a href='mailto:bauzalarrosasantiago@gmail.com' target='_blank'>
-        <BsFillEnvelopeAtFill id={style.mail_icon} />
-      </a>
-      <a href='https://www.instagram.com/santii.larrosa/' target='_blank'>
-        <BsInstagram />
-      </a>
-      <a href='https://wa.me/+59897111899' target='_blank'>
-        <BsWhatsapp />
-      </a>
-    </div>
-  );
+    return (
+        <div className={style.social}>
+            <a id="githubTest" href="https://github.com/Dev-Santi" target="_blank">
+                <BsGithub />
+            </a>
+            <a
+                id="linkedinTest"
+                href="https://www.linkedin.com/in/santiago-larrosa-bauz%C3%A1-99b740251/"
+                target="_blank"
+            >
+                <BsLinkedin />
+            </a>
+            <a id="emailTest" href="mailto:bauzalarrosasantiago@gmail.com" target="_blank">
+                <BsFillEnvelopeAtFill id={style.mail_icon} />
+            </a>
+            <a id="instagramTest" href="https://www.instagram.com/santii.larrosa/" target="_blank">
+                <BsInstagram />
+            </a>
+            <a id="whatsappTest" href="https://wa.me/+59897111899" target="_blank">
+                <BsWhatsapp />
+            </a>
+        </div>
+    );
 }
 
 export default Social;
