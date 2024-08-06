441d409c22ac98df725e5fea2ece52e61d6969b6
Dev-Santi <bauzalarrosasantiago@gmail.com>
2024-08-06 19:46:49 -0300

    changes

--- src/components/Header/Indices.jsx
+++ src/components/Header/Indices.jsx
@@ -2,41 +2,42 @@ import style from "./header.module.css";
 import { useState, useEffect } from "react";
 
 function Indices() {
-    const [scroll, setScroll] = useState(0);
+   const [scroll, setScroll] = useState(0);
 
-    console.log(scroll);
+   console.log(scroll);
 
-    function updateScroll() {
-        setScroll(window.scrollY);
-    }
+   function updateScroll() {
+      setScroll(window.scrollY);
+   }
+   setScroll(window.scrollY);
 
-    useEffect(() => {
-        window.addEventListener("scroll", updateScroll);
+   useEffect(() => {
+      window.addEventListener("scroll", updateScroll);
 
-        //Al desmontarse el componente, se elimina el evento para no generar un loop
-        return function () {
-            window.removeEventListener("scroll", updateScroll);
-        };
-    }, []);
+      //Al desmontarse el componente, se elimina el evento para no generar un loop
+      return function () {
+         window.removeEventListener("scroll", updateScroll);
+      };
+   }, []);
 
-    return (
-        <div className={style.indice}>
-            <ul>
-                <li className={scroll <= 1000 ? style.active : ""}>
-                    <span></span>
-                    <a href="#">
-                        <p>PROYECTOS</p>
-                    </a>
-                </li>
-                <li className={scroll > 1000 ? style.active : ""}>
-                    <span></span>
-                    <a href="#about">
-                        <p>SOBRE MÍ</p>
-                    </a>
-                </li>
-            </ul>
-        </div>
-    );
+   return (
+      <div className={style.indice}>
+         <ul>
+            <li className={scroll <= 1000 ? style.active : ""}>
+               <span></span>
+               <a href="#">
+                  <p>PROYECTOS</p>
+               </a>
+            </li>
+            <li className={scroll > 1000 ? style.active : ""}>
+               <span></span>
+               <a href="#about">
+                  <p>SOBRE MÍ</p>
+               </a>
+            </li>
+         </ul>
+      </div>
+   );
 }
 
 export default Indices;
