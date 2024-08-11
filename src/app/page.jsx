def045b0865e2af8dae542ddefc7454445cd8127
Dev-Santi <bauzalarrosasantiago@gmail.com>
2024-08-11 16:05:18 -0300

    new background

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -9,33 +9,29 @@ import Projects from "@/components/Projects";
 import PointerLight from "@/components/PointerLight";
 
 export default function Home() {
-    const [pointer, setPointer] = useState({ x: -100, y: 0 });
-    const { x, y } = pointer;
+   const [pointer, setPointer] = useState({ x: -100, y: 0 });
+   const { x, y } = pointer;
 
-    function pointerOut() {
-        setPointer({ x: -100, y: 0 });
-    }
+   function pointerOut() {
+      setPointer({ x: -100, y: 0 });
+   }
 
-    function updatePointer(e) {
-        setPointer({ x: e.clientX, y: e.clientY });
-    }
+   function updatePointer(e) {
+      setPointer({ x: e.clientX, y: e.clientY });
+   }
 
-    return (
-        <body
-            onMouseMove={updatePointer}
-            onMouseLeave={pointerOut}
-            className={poppins.className + " overflow_hidden"}
-        >
-            <PointerLight x={x} y={y} />
-            <main>
-                <section>
-                    <Header />
-                </section>
-                <aside>
-                    <Projects />
-                    <About />
-                </aside>
-            </main>
-        </body>
-    );
+   return (
+      <body onMouseMove={updatePointer} onMouseLeave={pointerOut} className={poppins.className + " overflow_hidden"}>
+         <PointerLight x={x} y={y} />
+         <main>
+            <section>
+               <Header />
+            </section>
+            <aside>
+               <Projects />
+               <About />
+            </aside>
+         </main>
+      </body>
+   );
 }
