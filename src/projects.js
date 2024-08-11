21bc9f9266e894bc1a46ede1f0717d41fcce4c54
Dev-Santi <bauzalarrosasantiago@gmail.com>
2024-08-11 13:15:53 -0300

    changes

--- src/projects.js
+++ src/projects.js
@@ -2,8 +2,7 @@ export default [
     {
         id: "9",
         title: "C&K Estudio de Danza",
-        description:
-            "Trabajo freeLance - Sitio web desarrollado para una escuela de danza",
+        description: "Trabajo freeLance - Sitio web desarrollado para una escuela de danza",
         img: "/card_imgs/CYK.png",
         alt: "Ilustración de cómo se ve el sitio web en una notebook.",
         tools: ["HTML", "CSS", "Vanilla JS", "Github Pages"],
@@ -12,42 +11,17 @@ export default [
     },
     {
         id: "8",
-        title: "Justo a Tiempo",
-        description:
-            "Aplicación web Full Stack que tiene como objetivo principal el facilitar una gestión eficiente de los plazos legales y administrativos en Uruguay.",
-        img: "/card_imgs/justo_a_tiempo.png",
-        alt: "Ilustración de cómo se ve el sitio web en una notebook.",
-        tools: [
-            "Next.js",
-            "MongoDB",
-            "Auth.js",
-            "Node.js",
-            "TypeScript",
-            "Tailwind",
-        ],
-        github: "https://github.com/Dev-Santi/LegalDateNextjs",
-        website: "https://www.justoatiempo.pro",
-    },
-    {
-        id: "7",
         title: "Taller Mecánico",
         description:
             "Aplicación web para un taller de mecánicos. Muestra de servicios, métodos de contacto a través de api, google maps.",
         img: "/card_imgs/taller.png",
         alt: "Ilustración de cómo se ve el sitio web en una notebook.",
-        tools: [
-            "TypeScript",
-            "Next.js",
-            "Node.js",
-            "Tailwind",
-            "Animate.css",
-            "Maps",
-        ],
+        tools: ["TypeScript", "Next.js", "Node.js", "Tailwind", "Maps"],
         github: "https://github.com/Dev-Santi/Taller-mec-nico-Next.js",
         website: "https://taller-mecanico-next-js.vercel.app/",
     },
     {
-        id: "6",
+        id: "7",
         title: "Restaurante",
         description:
             "Aplicación web para un restaurante, con la funcionalidad de poder hacer reservas desde el sitio o descargar el menu del día.",
@@ -58,7 +32,7 @@ export default [
         website: "https://rest-dev-santi.vercel.app/",
     },
     {
-        id: "5",
+        id: "6",
         title: "Portafolio para Fotógrafos",
         description:
             "Galería web completa para fotógrafos y/o diseñadores que deseen tener su propio espacio para mostrar su trabajo.",
@@ -67,6 +41,16 @@ export default [
         tools: ["Next.js", "Tailwind"],
         github: "https://github.com/Dev-Santi/photographer_portfolio",
         website: "https://photographer-portfolio-smoky.vercel.app/",
+    },
+    {
+        id: "5",
+        title: "Brascesco & Larrosa",
+        description: "Sitio web para un estudio jurídico ficticio.",
+        img: "/card_imgs/brascesco_larrosa.png",
+        alt: "Ilustración de cómo se ve el sitio web en una notebook.",
+        tools: ["HTML", "SASS", "JavaScript"],
+        github: "https://github.com/Dev-Santi/coderhouse--BrascescoyLarrosa",
+        website: "https://dev-santi.github.io/coderhouse--BrascescoyLarrosa/index.html",
     },
     {
         id: "4",
@@ -79,28 +63,26 @@ export default [
         github: "https://github.com/Dev-Santi/manicura-site",
         website: "https://manicura-site.vercel.app/",
     },
-
     {
         id: "3",
+        title: "Justo a Tiempo",
+        description:
+            "Aplicación web Full Stack que tiene como objetivo principal el facilitar una gestión eficiente de los plazos legales y administrativos en Uruguay.",
+        img: "/card_imgs/justo_a_tiempo.png",
+        alt: "Ilustración de cómo se ve el sitio web en una notebook.",
+        tools: ["Next.js", "MongoDB", "Node.js", "TypeScript", "Tailwind"],
+        github: "https://github.com/Dev-Santi/LegalDateNextjs",
+        website: "https://www.justoatiempo.pro",
+    },
+    {
+        id: "2",
         title: "Portafolio para diseñadores",
         description: "Portada para un portafolio web destinado a diseñadores.",
         img: "/card_imgs/portfolio.png",
         alt: "Ilustración de cómo se ve el sitio web en una notebook.",
         tools: ["HTML", "CSS", "JavaScript"],
         github: "https://github.com/Dev-Santi/challenges--Single-page-portfolio",
-        website:
-            "https://dev-santi.github.io/challenges--Single-page-portfolio",
-    },
-    {
-        id: "2",
-        title: "Brascesco & Larrosa",
-        description: "Sitio web para un estudio jurídico ficticio.",
-        img: "/card_imgs/brascesco_larrosa.png",
-        alt: "Ilustración de cómo se ve el sitio web en una notebook.",
-        tools: ["HTML", "SASS", "BootStrap"],
-        github: "https://github.com/Dev-Santi/coderhouse--BrascescoyLarrosa",
-        website:
-            "https://dev-santi.github.io/coderhouse--BrascescoyLarrosa/index.html",
+        website: "https://dev-santi.github.io/challenges--Single-page-portfolio",
     },
     {
         id: "1",
@@ -115,8 +97,7 @@ export default [
     {
         id: "0",
         title: "Skilled E-Learning",
-        description:
-            "Landing page para empresa ficticia que brinda servicios educativos online.",
+        description: "Landing page para empresa ficticia que brinda servicios educativos online.",
         img: "/card_imgs/skilled_elearning.png",
         alt: "Ilustración de cómo se ve el sitio web en una notebook.",
         tools: ["HTML", "SASS"],
