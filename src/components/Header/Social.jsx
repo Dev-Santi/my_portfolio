229e3be690cc87c231fc4e7ad49f710d4eae95c1
Dev-Santi <bauzalarrosasantiago@gmail.com>
2024-08-11 21:16:09 -0300

    changes on mailto

--- src/components/Header/Social.jsx
+++ src/components/Header/Social.jsx
@@ -1,40 +1,34 @@
 import style from "./header.module.css";
-import {
-    BsInstagram,
-    BsGithub,
-    BsLinkedin,
-    BsFillEnvelopeAtFill,
-    BsWhatsapp,
-} from "react-icons/bs";
+import { BsInstagram, BsGithub, BsLinkedin, BsFillEnvelopeAtFill, BsWhatsapp } from "react-icons/bs";
 
 function Social() {
-    return (
-        <div className={style.social}>
-            <a data-testid="github" href="https://github.com/Dev-Santi" target="_blank">
-                <BsGithub />
-            </a>
-            <a
-                data-testid="linkedin"
-                href="https://www.linkedin.com/in/santiago-larrosa-bauz%C3%A1-99b740251/"
-                target="_blank"
-            >
-                <BsLinkedin />
-            </a>
-            <a data-testid="email" href="mailto:bauzalarrosasantiago@gmail.com" target="_blank">
-                <BsFillEnvelopeAtFill id={style.mail_icon} />
-            </a>
-            <a
-                data-testid="instagram"
-                href="https://www.instagram.com/santii.larrosa/"
-                target="_blank"
-            >
-                <BsInstagram />
-            </a>
-            <a data-testid="whatsapp" href="https://wa.me/+59897111899" target="_blank">
-                <BsWhatsapp />
-            </a>
-        </div>
-    );
+   return (
+      <div className={style.social}>
+         <a data-testid="github" href="https://github.com/Dev-Santi" target="_blank">
+            <BsGithub />
+         </a>
+         <a
+            data-testid="linkedin"
+            href="https://www.linkedin.com/in/santiago-larrosa-bauz%C3%A1-99b740251/"
+            target="_blank"
+         >
+            <BsLinkedin />
+         </a>
+         <a
+            data-testid="email"
+            href="mailto:info@santiagolarrosa.tech?subject=Asesoramiento.&body=Mi consulta es la siguiente: "
+            target="_blank"
+         >
+            <BsFillEnvelopeAtFill id={style.mail_icon} />
+         </a>
+         <a data-testid="instagram" href="https://www.instagram.com/santii.larrosa/" target="_blank">
+            <BsInstagram />
+         </a>
+         <a data-testid="whatsapp" href="https://wa.me/+59897111899" target="_blank">
+            <BsWhatsapp />
+         </a>
+      </div>
+   );
 }
 
 export default Social;
