d9be1b325b06e3a2492810f111eb39a0ab55a6bc
Dev-Santi <bauzalarrosasantiago@gmail.com>
2024-09-17 22:40:57 -0300

    update

--- src/components/Header/Title.jsx
+++ src/components/Header/Title.jsx
@@ -3,7 +3,7 @@ import styles from "./header.module.css";
 function Title() {
    return (
       <h1>
-         Santiago Larrosa <a target="_blank" href="https://www.instagram.com/santii.larrosa">@web_montevideo</a>
+         Santiago Larrosa <a target="_blank" href="https://www.instagram.com/web_montevideo">@web_montevideo</a>
       </h1>
    );
 }
